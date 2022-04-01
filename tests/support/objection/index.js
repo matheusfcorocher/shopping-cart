f096c34dbb425ad73407bc31bae2aa87b5514efb
Matheus <matheuscorocher@gmail.com>
2022-04-01 17:06:31 -0300

    refactor: remove all .js files that was generated by tsconfig

--- tests/support/objection/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const objection_1 = require("objection");
-function mockModel(model) {
-    const query = objection_1.QueryBuilder.forClass(model);
-    jest.spyOn(model, "query").mockReturnValue(query);
-    return query;
-}
-exports.default = mockModel;
