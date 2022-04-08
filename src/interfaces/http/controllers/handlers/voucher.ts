9467ee2b0a44569966ef375d115368862a0596ed
Matheus <matheuscorocher@gmail.com>
2022-04-08 10:01:23 -0300

    feat: add test for listVouchers route

--- src/interfaces/http/controllers/handlers/voucher.ts
+++ src/interfaces/http/controllers/handlers/voucher.ts
@@ -1,19 +1,32 @@
-import { FastifyRequest, FastifyReply } from 'fastify';
-import { VoucherSerializer } from '../serializers/VoucherSerializer';
+import { FastifyRequest, FastifyReply } from "fastify";
+import { HttpResponseError } from "../../../../lib/CustomError";
+import { VoucherSerializer } from "../serializers/VoucherSerializer";
 
-const getVouchersListHandler = async (req: FastifyRequest, reply: FastifyReply) => {
-    try {
-      const { listVouchers } = req.container.vouchers;
-      const result = await listVouchers.execute();
-      console.log(result.map(r => VoucherSerializer.serialize(r)));
-      reply.send(result.map(r => VoucherSerializer.serialize(r)));
-    } catch (error : any) {
-      switch (error.CODE) {
-        default:
-          const { message, details } = error;
-          return reply.status(500).send({ message, details });
-      }
+const getVouchersListHandler = async (
+  req: FastifyRequest,
+  reply: FastifyReply
+) => {
+  try {
+    const { listVouchers } = req.container.vouchers;
+    const result = await listVouchers.execute();
+    console.log(result.map((r) => VoucherSerializer.serialize(r)));
+    reply.send(result.map((r) => VoucherSerializer.serialize(r)));
+  } catch (error: any) {
+    const httpResponseError = new HttpResponseError({
+      title: error.title,
+      status: error.status,
+      message: error.message,
+      detail: error.detail,
+    });
+    switch (error.status) {
+      case 404:
+        return reply.status(404).send(httpResponseError.toJson());
+      default:
+        httpResponseError.title = "Internal Server Error";
+        httpResponseError.status = 500;
+        return reply.status(500).send(httpResponseError.toJson());
     }
-  };
-  
-  export { getVouchersListHandler };
+  }
+};
+
+export { getVouchersListHandler };
---
+++ tests/integration/routes/vouchers/listVouchersRoute.test.ts
@@ -0,0 +1,83 @@
+import supertest from "supertest";
+import { app } from "../../../../app";
+import { VoucherModel } from "../../../../src/infra/database/knex/models/VoucherModel";
+import VoucherModelFactory from "../../../support/factories/models/VoucherModelFactory";
+import mockModel from "../../../support/objection";
+
+const { setupIntegrationTest } = require("../../../support/setup");
+
+describe("Interfaces :: Voucher :: Routes :: ListVouchers", () => {
+  describe("API :: GET /api/vouchers", () => {
+    setupIntegrationTest();
+    beforeEach(async () => {
+      await VoucherModelFactory.createList([
+        {
+          uuid: "7ea29c37-f9e7-4453-bc58-50ed4b5c0fcf",
+          code: "TEST1",
+          type: "percentual",
+          amount: 4000,
+        },
+        {
+          uuid: "92d91715-34ad-449e-9b81-73f1a74ef44e",
+          code: "TEST2",
+          type: "fixed",
+          amount: 4000,
+        },
+        {
+          uuid: "8bc94226-3e20-40cb-a507-554fabf36ffa",
+          code: "TEST3",
+          type: "free shipping",
+          amount: 2,
+          minValue: 5000,
+        },
+      ]);
+    });
+
+    describe("When make a request", () => {
+      it("returns correct vouchers", async () => {
+        const response = await supertest(app.server)
+          .get("/api/vouchers")
+          .expect(200);
+        const expected = [
+          {
+            id: "7ea29c37-f9e7-4453-bc58-50ed4b5c0fcf",
+            code: "TEST1",
+            type: "percentual",
+            amount: 40,
+          },
+          {
+            id: "92d91715-34ad-449e-9b81-73f1a74ef44e",
+            code: "TEST2",
+            type: "fixed",
+            amount: 40,
+          },
+          {
+            id: "8bc94226-3e20-40cb-a507-554fabf36ffa",
+            code: "TEST3",
+            type: "free shipping",
+            amount: 0.02,
+            minValue: 50,
+          },
+        ];
+
+        expect(response.body).toEqual(expect.arrayContaining(expected));
+      });
+    });
+  });
+  describe("When service is unavailable", () => {
+    it("returns error", async () => {
+      const error = new Error("Service Unavailable");
+      mockModel(VoucherModel).reject(error);
+      const response = await supertest(app.server)
+        .get("/api/vouchers")
+        .expect(500);
+      jest.restoreAllMocks();
+
+      expect(response.body).toEqual({
+        title: "Internal Server Error",
+        status: 500,
+        message: "Service Unavailable",
+      });
+    });
+  });
+});
